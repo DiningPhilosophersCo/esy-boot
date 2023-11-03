9163ddff73e677ef048084adc86480c4c859972e
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-11-03 17:14:46 +0530

    Adds support for namespaces other than @opam/

--- scripts/install-artifacts.sh
+++ scripts/install-artifacts.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 ENV_FILE="$1"
 PATH_FILE="$2"
@@ -12,7 +12,8 @@ if [ -d _build/install/default ]
 then
     rm -rf "$PREFIX"
     cp -L -R _build/install/default "$PREFIX"
-elif [ -f *.install ]
+elif [ -f *.install ] # TODO -f maynot work on globs everywhere 
 then
-    env -i -P "$(cat $PATH_FILE)" -S "$(cat $ENV_FILE)" "$ESY_INSTALLER_PATH" "./${PACKAGE_NAME#@opam/}.install" | bash
+    PACKAGE_NAME=$(echo "$PACKAGE_NAME" | sed -E 's/@[^\/]+\///g') # ${PACKAGE_NAME#@opam/} wasn't good enough. Users could have custom namespaces on NPM. Eg: @grain
+    env -i -P "$(cat $PATH_FILE)" -S "$(cat $ENV_FILE)" "$ESY_INSTALLER_PATH" "./$PACKAGE_NAME.install" | bash
 fi
