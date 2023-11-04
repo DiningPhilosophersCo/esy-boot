29a24a3b15fc4123618565165949ffe17964feb0
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-11-04 09:14:09 +0530

    Install all .install files. Not just <packageName>.install

--- scripts/install-artifacts.sh
+++ scripts/install-artifacts.sh
@@ -6,14 +6,14 @@ ENV_FILE="$1"
 PATH_FILE="$2"
 ESY_INSTALLER_PATH="$3"
 PREFIX="$4"
-PACKAGE_NAME="$5"
+# PACKAGE_NAME="$5" # TODO stop passing the this argument from caller
 
 if [ -d _build/install/default ]
 then
     rm -rf "$PREFIX"
     cp -L -R _build/install/default "$PREFIX"
-elif [ -f *.install ] # TODO -f maynot work on globs everywhere 
-then
-    PACKAGE_NAME=$(echo "$PACKAGE_NAME" | sed -E 's/@[^\/]+\///g') # ${PACKAGE_NAME#@opam/} wasn't good enough. Users could have custom namespaces on NPM. Eg: @grain
-    env -i -P "$(cat $PATH_FILE)" -S "$(cat $ENV_FILE)" "$ESY_INSTALLER_PATH" "./$PACKAGE_NAME.install" | bash
+else
+    # ${PACKAGE_NAME#@opam/} isn't good enough. Users could have custom namespaces on NPM. Eg: @grain
+    find "${ENV_FILE%.env}" -name '*.install' -exec env -i -P "$(cat "$PATH_FILE")" -S "$(cat "$ENV_FILE")" "$ESY_INSTALLER_PATH" {} + | bash -x
 fi
+
