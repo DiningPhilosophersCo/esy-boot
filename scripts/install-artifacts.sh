79814e8670e32b1320b84c51209fe60fbfe9c695
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-11-06 12:55:35 +0530

    Fix find exec running only once

--- scripts/install-artifacts.sh
+++ scripts/install-artifacts.sh
@@ -8,12 +8,5 @@ ESY_INSTALLER_PATH="$3"
 PREFIX="$4"
 # PACKAGE_NAME="$5" # TODO stop passing the this argument from caller
 
-if [ -d _build/install/default ]
-then
-    rm -rf "$PREFIX"
-    cp -L -R _build/install/default "$PREFIX"
-else
-    # ${PACKAGE_NAME#@opam/} isn't good enough. Users could have custom namespaces on NPM. Eg: @grain
-    find "${ENV_FILE%.env}" -name '*.install' -exec env -i -P "$(cat "$PATH_FILE")" -S "$(cat "$ENV_FILE")" "$ESY_INSTALLER_PATH" {} + | bash -x
-fi
+find "${ENV_FILE%.env}" -name '*.install' -exec env -i -P "$(cat "$PATH_FILE")" -S "$(cat "$ENV_FILE")" "$ESY_INSTALLER_PATH" {} ';' | bash -x
 
